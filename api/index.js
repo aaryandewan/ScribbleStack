9d7b61b81be72b8ea96782ae303cf9a909ef52da
Aaryan Dewan <aaryan.dewan@iongroup.com>
2023-06-27 15:08:25 +0530

    connected mongobd

--- api/index.js
+++ api/index.js
@@ -1,11 +1,30 @@
+require("dotenv").config({ path: "../.env" });
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyParser.json());
 
 const cors = require("cors");
 app.use(cors());
+let passw = process.env.MONGODB_PASSWORD;
+mongoose
+  .connect(
+    "mongodb+srv://aaryan:" +
+      passw +
+      "@cluster0.ldq41v4.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 app.post("/api/register", (req, res) => {
   const { username, password } = req.body;
